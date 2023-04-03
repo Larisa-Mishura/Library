2c4189439a2aefcd26e9835902bad04f3cac5f5c
Larisa <mlarisa7777@gmail.com>
2023-04-03 15:45:29 +0300

    first commit

---
+++ src/main/java/com/mishura/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.mishura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
